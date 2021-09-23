2ff33976306f9f6a5e365ec298ea688bc6fc3bcd
huypham <huy.pv@haposoft.com>
2021-09-23 20:26:22 +0700

    frontend allcourses

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -1,13 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Courses;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     //
-    public function course() {
-        return view('all_courses');
+    public function create($data)
+    {
+        return Course::create([
+            'title' => $data['title'],
+            'logo_path' => $data['logo_path'],
+            'description' => $data['description'],
+            'intro' => $data['intro'],
+            'learn_time' => $data['learn_time'],
+            'quizzes' => $data['quizzes'],
+            'price' => $data['price'],
+        ]);
+    }
+
+    public function course()
+    {
+        $courses = DB::table('courses')->select('*');
+        $courses = $courses->get();
+
+        return view('all_courses', ['courses' => $courses]);
     }
 }
--- resources/views/all_courses.blade.php
+++ resources/views/all_courses.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container all-course">
-    <div class="d-flex">
+    <div id="btnFilter" class="d-flex mb-3">
         <div class="course-filter">
         <!-- <i class="fas fa-sliders-h">Filter</i> -->
             <p class="course-filter-text">Filter</p>
@@ -11,110 +11,95 @@
             <input class="course-search" type="text" placeholder="Search...">
             <i class="fas fa-search"></i>
         </div>
-        <a href="#" class="btn btn-primary btn-course-lookup">Tìm kiếm</a>
+        <a href="#" class="btn btn-primary btn-course-search">Tìm kiếm</a>
     </div>
-    <div class="course-list">
+    <div class="course-lookup d-none">
+        <div class="course-lookup-text">Lọc theo</div>
         <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex">
-                            <img class="course-list-img" src="./assets/img/ellipse_html.png" alt="ellipse_html">
-                            <div class="">
-                                <p class="course-list-title">HTML Fundamentals</p>
-                                <p class="course-list-intro">Practice during lessons, practice between lessons, practice whenever you can. Master the task, then reinforce and test your knowledge with fun, hands-on exercises and interactive quizzes.</p>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <a href="#" class="btn btn-primary course-list-btn">More</a>
-                        </div>
-                        <div class="d-flex flex-row justify-content-center course-list-statistic">
-                            <div class="col-4 p-0 d-flex flex-column align-items-start">
-                                <div class="course-list-statistic-title">Learners</div>
-                                <div class="course-list-statistic-number">1586</div>
-                            </div>
-                            <div class="col-4 p-0 d-flex flex-column align-items-center">
-                                <div class="course-list-statistic-title">Lessons</div>
-                                <div class="course-list-statistic-number">2,689</div>
-                            </div>
-                            <div class="col-4 p-0 d-flex flex-column align-items-end">
-                                <div class="mr-3 course-list-statistic-title">Times</div>
-                                <div class="course-list-statistic-number">100 (h)</div>
-                            </div>
-                        </div>
+            <a href="#" class="btn btn-primary mr-2 mb-3 course-lookup-btn-newest">Mới nhất</a>
+            <a href="#" class="btn btn-primary mr-2 mb-3 course-lookup-btn-oldest">Cũ nhất</a>
+            <div class="mr-2 course-lookup-dropdown">
+                <div class="dropdown show">
+                    <a class="btn btn-secondary course-lookup-dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Teacher
+                        <i class="fas fa-angle-down"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#">Tăng dần</a>
+                        <a class="dropdown-item" href="#">Giảm dần</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex">
-                            <img class="course-list-img" src="./assets/img/ellipse_html.png" alt="ellipse_html">
-                            <div class="">
-                                <p class="course-list-title">HTML Fundamentals</p>
-                                <p class="course-list-intro">Practice during lessons, practice between lessons, practice whenever you can. Master the task, then reinforce and test your knowledge with fun, hands-on exercises and interactive quizzes.</p>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <a href="#" class="btn btn-primary course-list-btn">More</a>
-                        </div>
-                        <div class="d-flex flex-row justify-content-center course-list-statistic">
-                            <div class="col-4 p-0 d-flex flex-column align-items-start">
-                                <div class="course-list-statistic-title">Learners</div>
-                                <div class="course-list-statistic-number">1586</div>
-                            </div>
-                            <div class="col-4 p-0 d-flex flex-column align-items-center">
-                                <div class="course-list-statistic-title">Lessons</div>
-                                <div class="course-list-statistic-number">2,689</div>
-                            </div>
-                            <div class="col-4 p-0 d-flex flex-column align-items-end">
-                                <div class="mr-3 course-list-statistic-title">Times</div>
-                                <div class="course-list-statistic-number">100 (h)</div>
-                            </div>
-                        </div>
+            <div class="mr-2 course-lookup-dropdown">
+                <div class="dropdown show">
+                    <a class="btn btn-secondary course-lookup-dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Số người học
+                        <i class="fas fa-angle-down"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#">Tăng dần</a>
+                        <a class="dropdown-item" href="#">Giảm dần</a>
+                    </div>
+                </div>
+            </div>
+            <div class="mr-2 course-lookup-dropdown">
+                <div class="dropdown show">
+                    <a class="btn btn-secondary course-lookup-dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Thời gian học
+                        <i class="fas fa-angle-down"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#">Tăng dần</a>
+                        <a class="dropdown-item" href="#">Giảm dần</a>
+                    </div>
+                </div>
+            </div><div class="mr-2 course-lookup-dropdown">
+                <div class="dropdown show">
+                    <a class="btn btn-secondary course-lookup-dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Số bài học
+                        <i class="fas fa-angle-down"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#">Tăng dần</a>
+                        <a class="dropdown-item" href="#">Giảm dần</a>
+                    </div>
+                </div>
+            </div><div class="mr-2 course-lookup-dropdown">
+                <div class="dropdown show">
+                    <a class="btn btn-secondary course-lookup-dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Tags
+                        <i class="fas fa-angle-down"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#">Tăng dần</a>
+                        <a class="dropdown-item" href="#">Giảm dần</a>
+                    </div>
+                </div>
+            </div><div class="mr-2 course-lookup-dropdown">
+                <div class="dropdown show">
+                    <a class="btn btn-secondary course-lookup-dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Review
+                        <i class="fas fa-angle-down"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#">Tăng dần</a>
+                        <a class="dropdown-item" href="#">Giảm dần</a>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="course-list">  
         <div class="row">
-        <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex">
-                            <img class="course-list-img" src="./assets/img/ellipse_html.png" alt="ellipse_html">
-                            <div class="">
-                                <p class="course-list-title">HTML Fundamentals</p>
-                                <p class="course-list-intro">Practice during lessons, practice between lessons, practice whenever you can. Master the task, then reinforce and test your knowledge with fun, hands-on exercises and interactive quizzes.</p>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <a href="#" class="btn btn-primary course-list-btn">More</a>
-                        </div>
-                        <div class="d-flex flex-row justify-content-center course-list-statistic">
-                            <div class="col-4 p-0 d-flex flex-column align-items-start">
-                                <div class="course-list-statistic-title">Learners</div>
-                                <div class="course-list-statistic-number">1586</div>
-                            </div>
-                            <div class="col-4 p-0 d-flex flex-column align-items-center">
-                                <div class="course-list-statistic-title">Lessons</div>
-                                <div class="course-list-statistic-number">2,689</div>
-                            </div>
-                            <div class="col-4 p-0 d-flex flex-column align-items-end">
-                                <div class="mr-3 course-list-statistic-title">Times</div>
-                                <div class="course-list-statistic-number">100 (h)</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        @foreach($courses as $key => $course)
             <div class="col-sm-6">
-                <div class="card">
+                <div class="card mb-5">
                     <div class="card-body">
                         <div class="d-flex">
-                            <img class="course-list-img" src="./assets/img/ellipse_html.png" alt="ellipse_html">
+                            <img class="course-list-img" src="{{$course->logo_path}}" alt="ellipse_html">
                             <div class="">
-                                <p class="course-list-title">HTML Fundamentals</p>
-                                <p class="course-list-intro">Practice during lessons, practice between lessons, practice whenever you can. Master the task, then reinforce and test your knowledge with fun, hands-on exercises and interactive quizzes.</p>
+                                <p class="course-list-title">{{$key}}</p>
+                                <p class="course-list-intro">{{$course->intro}}</p>
                             </div>
                         </div>
                         <div class="d-flex justify-content-end">
@@ -131,12 +116,13 @@
                             </div>
                             <div class="col-4 p-0 d-flex flex-column align-items-end">
                                 <div class="mr-3 course-list-statistic-title">Times</div>
-                                <div class="course-list-statistic-number">100 (h)</div>
+                                <div class="course-list-statistic-number">{{$course->learn_time}} (h)</div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        @endforeach
         </div>
     </div>
 </div>
