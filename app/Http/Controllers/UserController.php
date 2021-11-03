01dd62e1de972d5780858b8f12c0aff3bb3cbab8
hapo-huy <huy.pv@haposoft.com>
2021-11-03 09:39:01 +0700

    rebase commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Course;
-use App\Models\Tag;
 use App\Models\User;
-use App\Models\Lesson;
-use App\Models\CourseUser;
-use Carbon\Carbon;
-use App\Models\Program;
 use App\http\Requests\UserRequest;
-use Illuminate\Support\Facades\DB;
 use Auth;
 
 class UserController extends Controller
@@ -21,27 +14,21 @@ class UserController extends Controller
         $user = $profile;
 
         if (Auth::user()->id == $user->id) {
-            $courses = $user->courses;
-
-            return view('profile.show', compact('user', 'courses'));
+            return view('profile.show', compact('user'));
         } else {
             return "404";
         }
     }
 
-    public function edit(UserRequest $request, User $profile)
+    public function update(UserRequest $request, User $profile)
     {
         $user = $profile;
-        $user->editInfoUser($request, $user);
+        if (!is_null($request->file('image'))) {
+            $user->uploadImg($request, $user);
+        } else {
+            $user->updateInfo($request, $user);
+        }
 
         return back()->with('success', 'Edit successfully!');
-    }
-
-    public function store(Request $request)
-    {
-        $user = Auth::user();
-        $user->uploadImg($request, $user);
-        
-        return back();
     }
 }
